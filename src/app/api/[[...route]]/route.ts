888464c4603224d7b7c75c0ae7951cd80b88d70d
sekibuuun <akaunto.no.001@gmail.com>
2024-11-24 17:29:47 +0900

    npm run lint

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,10 @@
-import { D1Database } from '@cloudflare/workers-types';
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { drizzle } from 'drizzle-orm/d1';
-import { users } from '../../../../schema';
+import type { D1Database } from "@cloudflare/workers-types"
+import { drizzle } from "drizzle-orm/d1"
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+import { users } from "../../../../schema"
 
-export const runtime = 'edge';
+export const runtime = "edge"
 
 // This ensures c.env.DB is correctly typed
 type Bindings = {
@@ -14,16 +14,18 @@ type Bindings = {
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DB: D1Database;
+      DB: D1Database
     }
   }
 }
 
-const app = new Hono<{ Bindings: Bindings}>().basePath('/api')
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api")
 
 // Accessing D1 is via the c.env.YOUR_BINDING property
 app.get("/query/customers", async (c) => {
-  let { results } = await process.env.DB.prepare("SELECT * FROM customers").all()
+  const { results } = await process.env.DB.prepare(
+    "SELECT * FROM customers"
+  ).all()
   return c.json(results)
 })
 
@@ -31,25 +33,25 @@ app.get("/query/customers", async (c) => {
  * get users
  */
 app.get("/users", async (c) => {
-  const db = drizzle(process.env.DB);
-  const result = await db.select().from(users).all();
-  return c.json(result);
-});
+  const db = drizzle(process.env.DB)
+  const result = await db.select().from(users).all()
+  return c.json(result)
+})
 
 /**
  * create users
  */
 app.post("/users", async (c) => {
-  const params = await c.req.json<typeof users.$inferSelect>();
-  const db = drizzle(process.env.DB);
+  const params = await c.req.json<typeof users.$inferSelect>()
+  const db = drizzle(process.env.DB)
   const result = await db
     .insert(users)
     .values({
-      userName: params.userName,
+      userName: params.userName
     })
-    .execute();
-  return c.json(result);
-});
+    .execute()
+  return c.json(result)
+})
 
-export const GET = handle(app);
-export const POST = handle(app);
+export const GET = handle(app)
+export const POST = handle(app)
