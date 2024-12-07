e9594a202e508276a3c81cadbc9fb4d58585c5e8
sekibuuun <akaunto.no.001@gmail.com>
2024-12-07 18:27:16 +0900

    fix: 自動デプロイ

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+};
 
 export default nextConfig;
