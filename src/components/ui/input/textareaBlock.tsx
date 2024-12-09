76e06b8cf700587946f6d2821e67d2bad6986b17
sekibuuun <akaunto.no.001@gmail.com>
2024-12-09 16:17:25 +0900

    fix: placeholderの表示条件を修正

--- src/components/ui/input/textareaBlock.tsx
+++ src/components/ui/input/textareaBlock.tsx
@@ -1,29 +1,43 @@
 import type { Block } from "@/types/type"
 import type React from "react"
-import type { ChangeEvent, KeyboardEvent } from "react"
+import { type ChangeEvent, type KeyboardEvent, useState } from "react"
 export const TextareaBlock: React.FC<{
   block: Block
   onBlockClick: (blockId: string) => void
   onKeyDown: (e: KeyboardEvent<HTMLTextAreaElement>, blockId: string) => void
   onChange: (e: ChangeEvent<HTMLTextAreaElement>, blockId: string) => void
   setTextareaRef: (element: HTMLTextAreaElement | null, blockId: string) => void
-}> = ({ block, onBlockClick, onKeyDown, onChange, setTextareaRef }) => (
-  <div
-    className="min-h-[1rem] p-1 rounded cursor-text"
-    onClick={() => onBlockClick(block.id)}
-  >
-    <textarea
-      ref={(el) => setTextareaRef(el, block.id)}
-      className="w-full resize-none outline-none bg-transparent py-1"
-      value={block.content}
-      onChange={(e) => onChange(e, block.id)}
-      onKeyDown={(e) => onKeyDown(e, block.id)}
-      placeholder="Type something..."
-      rows={1}
-      style={{
-        overflow: "hidden",
-        lineHeight: "1.5"
-      }}
-    />
-  </div>
-)
+}> = ({ block, onBlockClick, onKeyDown, onChange, setTextareaRef }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
+
+  return (
+    <div
+      className="min-h-[1rem] p-1 rounded cursor-text"
+      onClick={() => onBlockClick(block.id)}
+    >
+      <textarea
+        ref={(el) => setTextareaRef(el, block.id)}
+        className="w-full resize-none outline-none bg-transparent py-1"
+        value={block.content}
+        onChange={(e) => onChange(e, block.id)}
+        onFocus={() => handleFocus()}
+        onBlur={() => handleBlur()}
+        onKeyDown={(e) => onKeyDown(e, block.id)}
+        placeholder={`${!isFocused ? "" : "文字を入力するか、「/」でコマンドを呼び出します..."}`}
+        rows={1}
+        style={{
+          overflow: "hidden",
+          lineHeight: "1.5"
+        }}
+      />
+    </div>
+  )
+}
