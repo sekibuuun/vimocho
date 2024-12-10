bef90b11487a80126b6102841ad6a86dff6ab1ab
sekibuuun <akaunto.no.001@gmail.com>
2024-12-10 15:35:33 +0900

    fix: エラーハンドリング

--- src/lib/utils/textareaUtils.ts
+++ src/lib/utils/textareaUtils.ts
@@ -9,8 +9,18 @@ export const findIndexBlocks = (blocks: Block[], blockId: string): number =>
   blocks.findIndex((block) => block.id === blockId)
 
 export const scrollElementIntoView = (elementId: string) => {
+  if (!elementId) return
   requestAnimationFrame(() => {
     const element = document.getElementById(elementId)
     element?.scrollIntoView({ behavior: "smooth", block: "end" })
+    if (!element) {
+      console.warn("要素が見つかりません")
+      return
+    }
+    try {
+      element.scrollIntoView({ behavior: "smooth", block: "end" })
+    } catch {
+      console.error("指定された要素までスクロールできませんでした")
+    }
   })
 }
