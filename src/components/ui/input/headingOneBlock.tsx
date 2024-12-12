31d03a25214d39a0ac7bd05ef4fbcbb0a70cacca
sekibuuun <akaunto.no.001@gmail.com>
2024-12-12 19:22:48 +0900

    feat: h1タグに変換

---
+++ src/components/ui/input/headingOneBlock.tsx
@@ -0,0 +1,33 @@
+import type { HeadingElementProps } from "@/types/type"
+import type React from "react"
+
+export const HeadingOneBlock: React.FC<HeadingElementProps> = ({
+  block,
+  onBlockClick,
+  onKeyDown,
+  onChange,
+  setBlockElementRef,
+  setIsComposing,
+  handleFocus,
+  handleBlur
+}) => (
+  <div
+    className="min-h-[2rem] p-1 rounded cursor-text"
+    onClick={() => onBlockClick(block.id)}
+  >
+    <h1
+      ref={(el) => setBlockElementRef(el, block.id)}
+      className="w-full outline-none bg-transparent py-1 text-3xl font-bold"
+      contentEditable
+      onChange={(e) => onChange(e, block.id)}
+      onFocus={() => handleFocus(block.id)}
+      onBlur={() => handleBlur(block.id)}
+      onKeyDown={(e) => onKeyDown(e, block.id)}
+      onCompositionStart={() => setIsComposing(true)}
+      onCompositionEnd={() => setIsComposing(false)}
+      suppressContentEditableWarning
+    >
+      {block.content}
+    </h1>
+  </div>
+)
