89ad07b35bab30024bc00cb3d0cde062f0b4dfd3
sekibuuun <akaunto.no.001@gmail.com>
2024-12-13 21:40:33 +0900

    fix: 共通化による型修正

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,8 +1,6 @@
-import type { ChangeEvent, FormEvent, KeyboardEvent } from "react"
+import type { ChangeEvent, KeyboardEvent } from "react"
 
-type BlockType = "textarea" | "headingOne"
-
-export type KindOfElementType = HTMLTextAreaElement | HTMLHeadingElement | null
+export type BlockType = "input" | "headingOne"
 
 export type Block = {
   id: string
@@ -11,28 +9,19 @@ export type Block = {
   type: BlockType
 }
 
-export type TextAreaElementMap = { [key: string]: HTMLTextAreaElement | null }
-
-export type HeadingElementMap = { [key: string]: HTMLHeadingElement | null }
-
-export type BlockElementMap = {
-  [key: string]: KindOfElementType
-}
+export type InputElementMap = { [key: string]: HTMLInputElement | null }
 
 export type BlockElementProps = {
   block: Block
   onBlockClick: (blockId: string) => void
-  onKeyDown: (e: KeyboardEvent<KindOfElementType>, blockId: string) => void
-  setBlockElementRef: (element: KindOfElementType, blockId: string) => void
+  onKeyDown: (e: KeyboardEvent<HTMLInputElement>, blockId: string) => void
+  onChange: (e: ChangeEvent<HTMLInputElement>, blockId: string) => void
+  setBlockElementRef: (
+    element: HTMLInputElement | null,
+    blockId: string
+  ) => void
   setIsComposing: (isComposing: boolean) => void
   isFocused?: boolean
   handleFocus: (blockId: string) => void
   handleBlur: (blockId: string) => void
-}
-
-export type TextAreaElementProps = BlockElementProps & {
-  onChange: (e: ChangeEvent<HTMLTextAreaElement>, blockId: string) => void
-}
-export type HeadingElementProps = BlockElementProps & {
-  onChange: (e: FormEvent<HTMLHeadingElement>, blockId: string) => void
 }
